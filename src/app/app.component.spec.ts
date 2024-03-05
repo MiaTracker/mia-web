ca4eb0894304b2677f44d6b6341ddb62ffae976b
NRA1 <NRA1adamt@gmail.com>
2024-03-05 15:47:22 +0100

    Renaming the project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -18,6 +18,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('mia-ui app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('mia-web app is running!');
   });
 });
